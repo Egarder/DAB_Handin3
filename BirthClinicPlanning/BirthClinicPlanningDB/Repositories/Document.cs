43795bf56cccddb5eb78f0388236c3b22c325d06
Camilla <camilla.holmstoel@hotmail.com>
2021-04-28 11:40:55 +0200

    IDocument og Document added

---
+++ BirthClinicPlanning/BirthClinicPlanningDB/Repositories/Document.cs
@@ -0,0 +1,17 @@
+﻿using BirthClinicPlanningDB.Repositories.RepositoryInterfaces;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthClinicPlanningDB.Repositories
+{
+    public abstract class Document : IDocument
+    {
+        public ObjectId Id { get; set; }
+
+        public DateTime CreatedAt => Id.CreationTime;
+    }
+}
