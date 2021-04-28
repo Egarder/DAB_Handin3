c2f4235f48e71f9d6cf890f1ff9664a037f6e8ac
Camilla <camilla.holmstoel@hotmail.com>
2021-04-28 11:47:17 +0200

    CollectionNaming

---
+++ BirthClinicPlanning/BirthClinicPlanningDB/Repositories/BsonCollectionAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthClinicPlanningDB.Repositories
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    class BsonCollectionAttribute : Attribute
+    {
+        public string CollectionName { get; }
+
+        public BsonCollectionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+    }
+}
