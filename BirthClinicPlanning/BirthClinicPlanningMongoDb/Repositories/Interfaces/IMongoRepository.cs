d69a05ddb822b272791fce3ae95bcf0661a5250c
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-28 12:00:51 +0200

    ss

--- BirthClinicPlanning/BirthClinicPlanningMongoDb/Repositories/Interfaces/IMongoRepository.cs
+++ BirthClinicPlanning/BirthClinicPlanningMongoDb/Repositories/Interfaces/IMongoRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using BirthClinicPlanningDB.Repositories.RepositoryInterfaces;
 
 namespace BirthClinicPlanningMongoDb.Repositories.Interfaces
 {
