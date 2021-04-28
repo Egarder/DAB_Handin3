145518db8d7cf07e34a0977deeaa0c6e8487c109
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-28 11:10:58 +0200

    aa

---
+++ BirthClinicPlanning/BirthClinicPlanningMongoDb/DomainObjects/Clinician.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthClinicPlanningDB.DomainObjects
+{
+    public class Clinician
+    {
+        [Key]
+        public int ClinicianID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Type { get; set; }
+
+        public ObservableCollection<Appointment> Appointments { get; set; }
+
+        [NotMapped]
+        public string Display
+        {
+            get => $"{Type} {FirstName} {LastName}";
+        }
+    }
+}
