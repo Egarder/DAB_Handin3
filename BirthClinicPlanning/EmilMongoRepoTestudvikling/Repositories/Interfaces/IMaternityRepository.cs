d15ee2ca0b04030721934570b50a9fa66f48e326
Camilla <camilla.holmstoel@hotmail.com>
2021-05-05 12:36:28 +0200

    MaternityRepo added

---
+++ BirthClinicPlanning/EmilMongoRepoTestudvikling/Repositories/Interfaces/IMaternityRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EmilMongoRepoTestudvikling.Domainmodels;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmilMongoRepoTestudvikling.Repositories.Interfaces
+{
+    public interface IMaternityRepository: IBaseRepository<MaternityRoom>
+    {
+        public ObservableCollection<MaternityRoom> GetAllMaternityRooms();
+
+        public MaternityRoom GetMaternityRoomWithSpecificNumber(int no);
+        public MaternityRoom GetSingleMaternityRoom(string id);
+
+        public void AddAppointmentToRoom(string roomid, Appointment appointment);
+
+        public void AddMaternity(MaternityRoom maternityRoom);
+    }
+}
