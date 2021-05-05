4529cc7d621d9525bf88d5c3383125c9803b424a
Camilla <camilla.holmstoel@hotmail.com>
2021-05-05 11:45:50 +0200

    ClinicianRepoAdded

---
+++ BirthClinicPlanning/EmilMongoRepoTestudvikling/Repositories/ClinicianRepository.cs
@@ -0,0 +1,35 @@
+﻿using BirthClinicPlanningMongoDbWebAPI.DomainObjects;
+using EmilMongoRepoTestudvikling.Repositories.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmilMongoRepoTestudvikling.Repositories
+{
+    public class ClinicianRepository : BaseRepository<Clinician>, IClinicianRepository
+    {
+        public ClinicianRepository(IMongoDbContext context) : base(context)
+        {
+        }
+        public void AddClinician(Clinician clinician)
+        {
+            _dbCollection.InsertOne(clinician);
+        }
+
+        public ObservableCollection<Clinician> GetAllClinicians()
+        {
+            return new ObservableCollection<Clinician>(_dbCollection.Find(new BsonDocument()).ToList());
+        }
+
+        public Clinician GetSingleClinician(string id)
+        {
+            var cliID = new ObjectId(id);
+            var filter = Builders<Clinician>.Filter.Eq(cli => cli.Id, cliID);
+            return _dbCollection.Find(filter).FirstOrDefault();
+        }
+    }
+}
