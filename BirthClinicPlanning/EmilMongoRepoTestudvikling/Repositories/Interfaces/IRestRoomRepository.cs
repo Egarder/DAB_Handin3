0edec16c1f196828f07ca2abf07b56ead8bd2668
Camilla <camilla.holmstoel@hotmail.com>
2021-05-05 12:21:25 +0200

    at

---
+++ BirthClinicPlanning/EmilMongoRepoTestudvikling/Repositories/Interfaces/IRestRoomRepository.cs
@@ -0,0 +1,24 @@
+﻿using BirthClinicPlanningMongoDbWebAPI.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmilMongoRepoTestudvikling.Repositories.Interfaces
+{
+    public interface IRestRoomRepository: IBaseRepository<RestRoom>
+    {
+        public ObservableCollection<RestRoom> GetAllRestRoom();
+
+        public RestRoom GetRestRoomWithSpecificNumber(int no);
+
+        public RestRoom GetSingleRestRoom(int id);
+
+        public void DelRestRoom(RestRoom restRoom);
+
+        public void AddAppointmentToRoom(int roomid, Appointment appointment);
+
+        public void AddRestRoom(RestRoom restRoom);
+    }
+}
---
+++ BirthClinicPlanning/EmilMongoRepoTestudvikling/Repositories/RestRoomRepository.cs
@@ -0,0 +1,46 @@
+﻿using BirthClinicPlanningMongoDbWebAPI.DomainObjects;
+using EmilMongoRepoTestudvikling.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmilMongoRepoTestudvikling.Repositories
+{
+    public class RestRoomRepository: BaseRepository<RestRoom>, IRestRoomRepository
+    {
+        public RestRoomRepository(IMongoDbContext context) : base(context)
+        {
+        }
+        public void AddAppointmentToRoom(int roomid, Appointment appointment)
+        {
+            //_dbCollection.InsertOne()
+        }
+
+        public void AddRestRoom(RestRoom restRoom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DelRestRoom(RestRoom restRoom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<RestRoom> GetAllRestRoom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RestRoom GetRestRoomWithSpecificNumber(int no)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RestRoom GetSingleRestRoom(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
